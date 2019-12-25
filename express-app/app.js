74407ea4646b424c9b63644b993daa49d42c66f1
Andrew Vo <voandrew@berkeley.edu>
2019-12-25 15:19:03 -0800

    Connected React frontend to Node backend.

--- express-app/app.js
+++ express-app/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var expressValidator = require('express-validator');
-var expressSession = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,10 +16,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
